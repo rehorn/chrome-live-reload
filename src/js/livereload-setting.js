f18153a1207db3887e2557fa2520d04d4d4401eb
rehorn <sysucgh@163.com>
2012-04-25 01:14:16 +0800

    option.html

---
+++ src/js/livereload-setting.js
@@ -0,0 +1,57 @@
+/**
+ * description: live reload background run init
+ * author: rehorn@vip.qq.com
+ * date: 2012-04-22
+ */
+
+(function(window, undefined){
+
+    var LiveReloadSetting = {
+        _option:{
+            lr_enable_css: true,
+            lr_enable_js: true,
+            lr_enable_html: true,
+            lr_skip_external: true,
+            lr_css_transitions: true,
+            lr_refresh_rate: 1000,
+            lr_enable_node_tag: true,
+            lr_live_list: []
+        },
+        set: function(key, value){
+            this._option[key] = value;
+            localStorage.setItem(key, JSON.stringify(value));
+        },
+        get: function(key){
+            return JSON.parse(localStorage.getItem(key)) || this._option[key];
+        },
+        remove: function(key){
+            localStorage.removeItem(key);
+        },
+        update: function(){
+            for(var key in this._option){
+                this._option[key] = this.get(key);
+            }
+        },
+        addLiveList: function(url){
+            var list = this._option['lr_live_list'];
+            list.push(url);
+            this.set('lr_live_list', list);
+        },
+        removeLiveList: function(url){
+            var list = this._option['lr_live_list'];
+            if(this.isUrlLive(url)){
+                list.remove(url);
+            }
+        },
+        isUrlLive: function(url){
+            return this._option['lr_live_list'].indexOf(url) > 0;
+        },
+        getOption: function(){
+            this.update();
+            return this._option;
+        }
+    };
+
+    window.LiveReloadSetting = LiveReloadSetting;
+
+})(window);
